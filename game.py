6c9277821f3fce33c15ae07a10e62ff949e53d99
RobertHelpa <130825260+RobertHelpa@users.noreply.github.com>
2023-11-02 21:39:55 +0200

    Add files via upload

---
+++ game.py
@@ -0,0 +1,11 @@
+from direct.showbase.ShowBase import ShowBase
+from mapmanager import MapManager
+
+class Game(ShowBase):
+    def __init__(self):
+        super().__init__()
+        self.land = MapManager()
+        base.camLens.setFov(90)
+
+Game().run()
+        
